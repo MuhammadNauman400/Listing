141b86afa3f4571f5d3aaf0eb475158c1d8973a9
MuhammadNauman400 <naumanmuhammad663@gmail.com>
2026-03-24 17:42:58 +0500

    Admin Sidebar Activation using global helper function

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+// Set Sidebar Active
+if (!function_exists('setSidebarActive')) {
+    function setSidebarActive(array $routes): ?string
+    {
+        foreach ($routes as $route) {
+            if (request()->routeIs($route)) {
+                return 'active';
+            }
+        }
+        return null;
+    }
+}
